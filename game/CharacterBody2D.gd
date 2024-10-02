b89df4d1d2d20b49647cc3e9be656434e1e9ae85
Fl0wwDev <mafradin@hotmail.fr>
2024-10-02 12:17:22 +0200

    ajout health bar

--- game/CharacterBody2D.gd
+++ game/CharacterBody2D.gd
@@ -1,14 +1,21 @@
 extends CharacterBody2D
 
-@export var speed = 400
+@onready var animation : AnimatedSprite2D = $AnimatedSprite2D
+@export var speed = 300
+var health = 50
+var health_max = 50
+var health_min = 0
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
+func _input(event):
+	if Input.is_key_pressed(KEY_Z) or Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_Q) or Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_S):
+		animation.play("walk")
+	else:
+		animation.play("idle")
+
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
-
-func _ready():
-	$AnimatedSprite2D.play()
---
+++ game/progress_bar.gd
@@ -0,0 +1,6 @@
+extends ProgressBar
+
+
+var parent
+var max_value_amount
+var min_value_amount
