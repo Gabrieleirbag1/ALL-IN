5bb5ca2398db0ba01d2aef13f27f3b0ea3b25d4e
Fl0wwDev <mafradin@hotmail.fr>
2024-10-02 14:08:22 +0200

    animation de mort

--- Script/CharacterBody2D.gd
+++ Script/CharacterBody2D.gd
@@ -2,20 +2,33 @@ extends CharacterBody2D
 
 @onready var animation : AnimatedSprite2D = $AnimatedSprite2D
 @export var speed = 300
-var health = 50
+var health = 3
 var health_max = 50
 var health_min = 0
+var alive : bool = true
+var death_animation_played : bool = false
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
+func check_health():
+	if health <= 0 and not death_animation_played:
+		alive = false
+		animation.play("death")
+		death_animation_played = true
+
 func _input(event):
-	if Input.is_key_pressed(KEY_Z) or Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_Q) or Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_S):
-		animation.play("walk")
-	else:
-		animation.play("idle")
+	if alive:
+		if Input.is_key_pressed(KEY_Z) or Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_Q) or Input.is_key_pressed(KEY_D):
+			animation.play("walk")
+			health -= 1
+			print(health)
+			check_health()
+		else:
+			animation.play("idle")
 
 func _physics_process(delta):
-	get_input()
-	move_and_slide()
+	if alive:
+		get_input()
+		move_and_slide()
