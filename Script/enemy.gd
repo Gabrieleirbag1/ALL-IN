e7c44dbef9c7e75f951a21fce59a4ac9c8a06b32
Gabriel <gabrielgarrone670@gmail.com>
2024-11-29 11:21:38 +0100

    Refactor slime behavior by creating a new enemy class; moved common functionality to enemy.gd for better code organization and maintainability.

---
+++ Script/enemy.gd
@@ -0,0 +1,63 @@
+# enemy.gd
+extends CharacterBody2D
+
+@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
+@export var speed = 30
+var player_chase = false
+var player = null
+var health = 50
+var health_max = 50
+var health_min = 0
+var alive: bool = true
+var death_animation_played: bool = false
+var immortal = false
+
+func play_animation(animation_name: String) -> void:
+	if not alive:
+		return
+	animation.play(animation_name)
+	
+func manage_collision(collision):
+	if collision:
+		var collider = collision.get_collider()
+		if collider.name == "Player":
+			if collider.alive:
+				collider.health -= 1
+				collider.animation.play("hurt")
+				collider.check_health()
+
+func chase_player(collision):
+	if player.position.x < position.x:
+		animation.flip_h = true
+	else:
+		animation.flip_h = false
+	manage_collision(collision)
+
+func check_health():
+	if immortal:
+		return
+	if health <= 0 and not death_animation_played:
+		alive = false
+		play_animation("death")
+		death_animation_played = true
+
+func _physics_process(delta: float) -> void:
+	if alive:
+		check_health()
+		if player_chase and player:
+			var direction = (player.position - position).normalized()
+			var collision = move_and_collide(direction * speed * delta)
+			chase_player(collision)
+			#rotation = position.angle_to(player.position)
+	
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.name == "Player":
+		player = body
+		player_chase = true
+		play_animation("walk")
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body == player:
+		player = null
+		player_chase = false
+		play_animation("idle")
--- Script/slime.gd
+++ Script/slime.gd
@@ -1,63 +1,13 @@
-extends CharacterBody2D
+# slime.gd
+extends "enemy.gd"
 
-@onready var animation: AnimatedSprite2D = $AnimatedSprite2D
-@export var speed = 30
-
-var player_chase = false
-var player = null
-var health = 50
-var health_max = 50
-var health_min = 0
-var alive: bool = true
-var death_animation_played: bool = false
-var immortal = false
-
-func play_animation(animation_name: String) -> void:
-	if not alive:
-		return
-	animation.play(animation_name)
-	
-func manage_collision(collision):
-	if collision:
-		var collider = collision.get_collider()
-		if collider.name == "Player":
-			if collider.alive:
-				collider.health -= 1
-				collider.animation.play("hurt")
-				collider.check_health()
-
-func chase_player(collision):
-	if player.position.x < position.x:
-		animation.flip_h = true
-	else:
-		animation.flip_h = false
-	manage_collision(collision)
-
-func check_health():
-	if immortal:
-		return
-	if health <= 0 and not death_animation_played:
-		alive = false
-		play_animation("death")
-		death_animation_played = true
-
-func _physics_process(delta: float) -> void:
-	if alive:
-		check_health()
-		if player_chase and player:
-			var direction = (player.position - position).normalized()
-			var collision = move_and_collide(direction * speed * delta)
-			chase_player(collision)
-			#rotation = position.angle_to(player.position)
-	
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body.name == "Player":
-		player = body
-		player_chase = true
-		play_animation("walk")
-
-func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body == player:
-		player = null
-		player_chase = false
-		play_animation("idle")
+func _init() -> void:
+	speed = 50
+	player_chase = false
+	player = null
+	health = 50
+	health_max = 50
+	health_min = 0
+	alive = true
+	death_animation_played = false
+	immortal = false
