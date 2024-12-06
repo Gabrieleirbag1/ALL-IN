9b46b1593d2039ec7a47c3c6026447ce8eda1937
Gabriel <gabrielgarrone670@gmail.com>
2024-12-06 13:52:53 +0100

    refactor: remove unused map.gd script and update player movement logic

--- Script/player.gd
+++ Script/player.gd
@@ -9,20 +9,14 @@ var health_min = 0
 var alive : bool = true
 var death_animation_played : bool = false
 var immortal = false
-
 var invincible = false
 
 func get_input():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
 	velocity = input_direction * speed
 
-	# Déclenche l'animation d'attaque si la touche est pressée
-	if Input.is_action_just_pressed("attack") and not is_attacking:
-		is_attacking = true
-		animation.play("attack_1")
-
 func _input(event):
-	if alive and not is_attacking:
+	if alive:
 		if Input.is_key_pressed(KEY_Z) or Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_DOWN):
 			animation.play("walk_shadow")
 		elif Input.is_key_pressed(KEY_RIGHT) or Input.is_key_pressed(KEY_D):
@@ -41,7 +35,7 @@ func check_health():
 		alive = false
 		animation.play("death")
 		death_animation_played = true
-		
+
 func take_damage(enemyVelocity, knockback_force, damage):
 	if not invincible:
 		health -= damage
@@ -57,14 +51,6 @@ func enemy_attack(velocity, knockback_force, damage):
 		invincible = true
 		invincibility_timer.start()
 
-
-func _input(event):
-	if alive:
-		if Input.is_key_pressed(KEY_Z) or Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_Q) or Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_UP) or Input.is_key_pressed(KEY_DOWN) or Input.is_key_pressed(KEY_LEFT) or Input.is_key_pressed(KEY_RIGHT):
-			animation.play("walk_shadow")
-		else:
-			animation.play("idle_shadow")
-			
 func _physics_process(delta):
 	if alive:
 		get_input()
@@ -72,10 +58,3 @@ func _physics_process(delta):
 
 func _on_invincibility_timeout() -> void:
 	invincible = false
-
-func _on_animation_finished():
-	# Réinitialise l'état après une attaque ou une mort
-	if animation.animation == "attack_1":
-		is_attacking = false
-	elif animation.animation == "death":
-		alive = false
