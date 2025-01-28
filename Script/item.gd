ffde3ede632009ba8f9c644898199709fd588cfa
Gabriel <gabrielgarrone670@gmail.com>
2025-01-28 19:49:05 +0100

    fix: refine drop detection logic for draggable items

--- Script/item.gd
+++ Script/item.gd
@@ -26,19 +26,16 @@ func _process(delta: float) -> void:
 			Global.is_dragging = false
 			var tween = get_tree().create_tween()
 			if is_inside_dropable:
-				print(body_ref.global_position)
 				tween.tween_property(self, "global_position", body_ref.global_position, 0.2).set_ease(Tween.EASE_OUT)
 			else:
 				tween.tween_property(self, "global_position", initialPos, 0.2).set_ease(Tween.EASE_OUT)
 
 func _on_area_2d_mouse_entered() -> void:
-	print("Mouse enter")
 	if not Global.is_dragging:
 		draggable = true
 		scale = Vector2(1.05, 1.05)
 
 func _on_area_2d_mouse_exited() -> void:
-	print("Mouse exit")
 	if not Global.is_dragging:
 		draggable = false
 		scale = Vector2(1, 1)
@@ -51,5 +48,6 @@ func _on_area_2d_body_entered(body: StaticBody2D) -> void:
 
 func _on_area_2d_body_exited(body: StaticBody2D) -> void:
 	if body.is_in_group('dropable'):
-		is_inside_dropable = false
-		body.modulate = Color(Color.REBECCA_PURPLE, 0.7)
+		if body_ref == body:
+			is_inside_dropable = false
+			body.modulate = Color(Color.REBECCA_PURPLE, 0.7)
