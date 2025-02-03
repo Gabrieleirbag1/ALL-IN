3000e17e6a0c5111ad10559de0baa25955e07a9d
Mahé FRADIN <mafradin@hotmail.fr>
2025-02-03 22:49:57 +0100

    feat: add orc rider enemy script and update references in scene

---
+++ Script/Enemies/orc_rider.gd
@@ -0,0 +1,16 @@
+# orc_rider.gd
+extends "enemy.gd"
+
+func _init() -> void:
+	drop_xp = 200
+	speed = 200
+	knockback_force = 2500
+	health = 300
+	health_max = 300
+	health_min = 0
+	damage = 10
+	alive = true
+	death_animation_played = false
+	immortal = false
+	player_chase = false
+	player = null
