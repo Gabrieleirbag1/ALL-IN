37fe04ff89b413f05f6b04a03ba06b7130f9ab83
Gabriel <gabrielgarrone670@gmail.com>
2025-02-22 14:56:12 +0100

    feat: add mouse click handling and shader setup to stat frame

--- Script/Stats/stat_frame.gd
+++ Script/Stats/stat_frame.gd
@@ -1,10 +1,29 @@
 extends Node2D
 
-var is_item_inside = false
+var is_mouse_inside = false
 var shader = false
 @onready var texture_rect: TextureRect = $TextureRect
 
 func _ready() -> void:
+	set_shader()
+	set_click_event()
+
+func _process(delta: float) -> void:
+	handle_click_action()
+
+func handle_click_action():
+	if Input.is_action_just_pressed("click"):
+		if is_mouse_inside:
+			print("unpause")
+
+func set_click_event():
+	if not InputMap.has_action("click"):
+		InputMap.add_action("click")
+		var mouse_button_event = InputEventMouseButton.new()
+		mouse_button_event.button_index = MOUSE_BUTTON_LEFT
+		InputMap.action_add_event("click", mouse_button_event)
+		
+func set_shader():
 	var shader = Shader.new()
 	shader.code = """
 	shader_type canvas_item;
@@ -20,7 +39,9 @@ func _ready() -> void:
 	texture_rect.material = mat
 
 func _on_area_2d_mouse_entered() -> void:
+	is_mouse_inside = true
 	texture_rect.material.set_shader_parameter("brightness", 25)
 
 func _on_area_2d_mouse_exited() -> void:
+	is_mouse_inside = false
 	texture_rect.material.set_shader_parameter("brightness", 1.0)
