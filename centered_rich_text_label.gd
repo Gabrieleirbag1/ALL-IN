682d81933dadaa6fd1377d54479b7f1696bd0f77
Gabriel <gabrielgarrone670@gmail.com>
2025-02-25 11:03:11 +0100

    feat: add type hints to get_centered_text and set_centered_text functions in CenteredRichTextLabel

--- centered_rich_text_label.gd
+++ centered_rich_text_label.gd
@@ -1,8 +1,8 @@
 class_name CenteredRichTextLabel extends RichTextLabel
 
-func get_centered_text(text):
-	var final_text = "[center]" + text + "[/center]"
+func get_centered_text(text: String) -> String:
+	var final_text: String = "[center]" + text + "[/center]"
 	return final_text
 	
-func set_centered_text(text):
+func set_centered_text(text: String):
 	self.text = get_centered_text(text)
