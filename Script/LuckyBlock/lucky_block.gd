c2b8a571656d38e7ad560aced444691b84f1c08a
Gabriel <gabrielgarrone670@gmail.com>
2025-02-28 16:21:24 +0100

    refactor: remove debug print statements from Lucky Block collision handling

--- Script/LuckyBlock/lucky_block.gd
+++ Script/LuckyBlock/lucky_block.gd
@@ -17,12 +17,9 @@ func _process(delta: float) -> void:
 	handle_open_action()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print(body)
 	if body is Player:
-		print(1)
 		has_player_entered = true
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body is Player:
-		print(2)
 		has_player_entered = false
