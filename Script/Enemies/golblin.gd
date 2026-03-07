3ae6273962f0f6de70e35eeaec92eaf026cf2cb3
Gabrieleirbag1 <gabrielgarrone670@gmail.com>
2026-03-07 23:27:55 +0100

    feat: attach custom script for new mobs

---
+++ Script/Enemies/golblin.gd
@@ -0,0 +1,17 @@
+# goblin.gd
+extends Enemy
+
+func _init() -> void:
+	drop_xp = 10
+	enemy_type = "goblin"
+	speed = 50
+	knockback_force = 1500
+	health = 150
+	health_max = 150
+	health_min = 0
+	damage = 2
+	ability_attack_damage = 7
+	ability_attack_cooldown = 2
+	min_ability_attack_frame = 6
+	max_ability_attack_frame = 7
+	
